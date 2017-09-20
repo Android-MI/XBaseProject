d10b8ddad84a7aaca88d37a3f67346f6eb9ebcc1
小米 <958936985@qq.com>
2017-09-20 17:33:00 +0800

    共通类提交

--- app/src/main/java/com/mi/xbaseprojectdemo/MainActivity.java
+++ app/src/main/java/com/mi/xbaseprojectdemo/MainActivity.java
@@ -1,27 +1,43 @@
 package com.mi.xbaseprojectdemo;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.mi.utils.CCLog;
 import com.mi.xbaseproject.BaseActivity;
+import com.mi.xbaseproject.common.view.CropCircleTransformation;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
 
+    @Nullable
+    @BindView(R.id.tv_text_sub_title)
+    TextView mTvSubTitle ;
+    @Nullable
+    @BindView(R.id.iv_text_sub_title)
+    ImageView mIvTest ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ButterKnife.bind(this);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        init();
+
+    }
+    private void init() {
+
         CCLog.e("Log Message");
+        mTvSubTitle.setText("HAHA");
+        Glide.with(this).load("http://upload-images.jianshu.io/upload_images/926747-e90b02a83e8aa382.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240").bitmapTransform(new CropCircleTransformation(this)).into(mIvTest);
     }
 
     @Override
     protected void resetLayout() {
-        TextView textView = findView(R.id.tv_text_sub_title);
-        textView.setText("NEW TITLE");
     }
 }
