210267832488d58f978d547845a77d861daa9ef1
小米 <958936985@qq.com>
2017-09-20 13:37:36 +0800

    first commit

---
+++ app/src/main/java/com/mi/xbaseprojectdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mi.xbaseprojectdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
